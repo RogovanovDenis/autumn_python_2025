c46e2a14fc28ed50863f04c04bce8729688370b1
RogovanovDenis <denis48144850@yandex.ru>
2025-10-26 16:20:07 +0300

    Version01

---
+++ task29.py
@@ -0,0 +1,20 @@
+#todo: Вы получаете из API список пользователей,
+# но нужно отфильтровать и преобразовать данные перед загрузкой в базу.
+# Создайте список email-адресов только для активных пользователей старше 18 лет.
+# Задачу следует решить с использованием списковых включений
+
+users = [
+    {"name": "alice", "email": "alice@example.com", "age": 25, "active": True},
+    {"name": "bob", "email": "bob@example.com", "age": 17, "active": True},
+    {"name": "charlie", "email": "charlie@example.com", "age": 30, "active": False},
+    {"name": "diana", "email": "diana@example.com", "age": 16, "active": True}
+]
+
+print([i["email"] for i in users if i["age"]  > 18 and i["active"] is True])
+
+
+
+
+
+#Результат
+['alice@example.com']
